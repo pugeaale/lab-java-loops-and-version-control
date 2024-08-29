74bafddbdad6e5e06c467c86f7836df5b2981e95
pugeaale <pugeaale@gmail.com>
2024-08-29 15:32:00 +0200

    :sparkles: adding task 1 of lab 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arrayTest = { 2 , -2 , 0 , 4};
+
+        System.out.println(getDiffBetweenLargestSmallest(arrayTest));
+    }
+
+    /*
+   Write a Java method that returns the difference between the largest and smallest values
+   in an array of integers.
+   The length of the array must be at least 1.
+     */
+    private static int getDiffBetweenLargestSmallest(int[] array) {
+        if( array == null || array.length == 0) {
+            return -1;
+        }
+        int smallest = array[0];
+        int largest = smallest;
+
+        for (int i = 1 ; i < array.length ; i++) {
+            if( array[i] > largest ) largest = array[i];
+            else if (array[i] < smallest) smallest = array[i];
+        }
+        return largest-smallest;
     }
 }
